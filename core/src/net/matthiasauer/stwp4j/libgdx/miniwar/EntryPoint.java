5d3a52cdf5d93785655765de5072f738a71c86ca
Matthias <mail@matthias-auer.net>
2015-12-14 19:31:21 +0100

    the example now works with html5

--- core/src/net/matthiasauer/stwp4j/libgdx/miniwar/EntryPoint.java
+++ core/src/net/matthiasauer/stwp4j/libgdx/miniwar/EntryPoint.java
@@ -17,7 +17,6 @@ public class EntryPoint extends ApplicationEntryPointProcess {
     @Override
     public void create() {
         scheduler.addProcess(new RenderProcess(Arrays.asList("data1.atlas"), true));
-
         scheduler.addProcess(new TestDataCreatorProcess(0, 0));
         scheduler.addProcess(new TestDataCreatorProcess(300, 200));
         scheduler.addProcess(new TestDataConsumerProcess());
