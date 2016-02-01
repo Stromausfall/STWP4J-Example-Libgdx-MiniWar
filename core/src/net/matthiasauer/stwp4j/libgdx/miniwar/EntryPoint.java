ef9459a1d7ee0774f1851e4a68995784d5ff3a6e
Matthias <mail@matthias-auer.net>
2016-02-01 18:50:24 +0100

    added code in the Desktop project to generate the texture atlas from a folder of images

--- core/src/net/matthiasauer/stwp4j/libgdx/miniwar/EntryPoint.java
+++ core/src/net/matthiasauer/stwp4j/libgdx/miniwar/EntryPoint.java
@@ -7,6 +7,8 @@ import net.matthiasauer.stwp4j.ChannelInPort;
 import net.matthiasauer.stwp4j.LightweightProcess;
 import net.matthiasauer.stwp4j.libgdx.application.ApplicationEntryPointProcess;
 import net.matthiasauer.stwp4j.libgdx.application.ApplicationEvent;
+import net.matthiasauer.stwp4j.libgdx.graphic.CameraChangeEvent;
+import net.matthiasauer.stwp4j.libgdx.graphic.CameraStatusEvent;
 import net.matthiasauer.stwp4j.libgdx.graphic.InputTouchEvent;
 import net.matthiasauer.stwp4j.libgdx.graphic.RenderData;
 import net.matthiasauer.stwp4j.libgdx.graphic.RenderPositionUnit;
@@ -41,44 +43,50 @@ public class EntryPoint extends ApplicationEntryPointProcess {
         Channel<WorldSnapShot> worldSnapShotChannel = this.scheduler.createMultiplexChannel("world-snapshot channel",
                 WorldSnapShot.class, true, false);
 
+        Channel<CameraStatusEvent> cameraStatusEventChannel = this.scheduler
+                .createMultiplexChannel("camera status event channel", CameraStatusEvent.class, false, true);
+        Channel<CameraChangeEvent> cameraChangeEventChannel = this.scheduler
+                .createMultiplexChannel("camera status change channel", CameraChangeEvent.class, false, true);
+
         // Plumbing - processes
         scheduler.addProcess(new RenderProcess(Arrays.asList("data1.atlas"), true, 640, 480,
-                ResizeBehavior.ChangeResolutionKeepAspect, renderDataChannel.createInPort(),
-                applicationEventChannel.createInPort(), inputTouchEventDataChannel.createOutPort()));
+                ResizeBehavior.KeepResolutionKeepAspect, renderDataChannel.createInPort(),
+                applicationEventChannel.createInPort(), inputTouchEventDataChannel.createOutPort(),
+                cameraChangeEventChannel.createInPort(), cameraStatusEventChannel.createOutPort()));
         scheduler.addProcess(
                 new WorldProcess(worldInteractionChannel.createInPort(), worldSnapShotChannel.createOutPort()));
 
+        scheduler.addProcess(
+                new ButtonProcess(renderDataChannel.createOutPort(), inputTouchEventDataChannel.createInPort(),
+                        buttonClickEventChannel.createOutPort(), new SpriteRenderData("2", 100, 0, 0,
+                                RenderPositionUnit.Percent, null, 0, false, "tile_grass"),
+                new SpriteRenderData("2", 100, 0, 0, RenderPositionUnit.Percent, null, 0, false, "tile_dirt"),
+                new SpriteRenderData("2", 100, 0, 0, RenderPositionUnit.Percent, null, 0, false, "tile_mountain")));
 
         scheduler.addProcess(new ButtonProcess(renderDataChannel.createOutPort(),
                 inputTouchEventDataChannel.createInPort(), buttonClickEventChannel.createOutPort(),
-                new SpriteRenderData().set("2", 100, 0, 0, RenderPositionUnit.Percent, null, 0, false, "tile_dirt"),
-                new SpriteRenderData().set("2", 100, 0, 0, RenderPositionUnit.Percent, null, 0, false, "tile_grass"),
-                new SpriteRenderData().set("2", 100, 0, 0, RenderPositionUnit.Percent, null, 0, false,
-                        "tile_mountain")));
-
-        scheduler.addProcess(new ButtonProcess(renderDataChannel.createOutPort(),
-                inputTouchEventDataChannel.createInPort(), buttonClickEventChannel.createOutPort(),
-                new SpriteRenderData().set("1", 0, 0, 0, RenderPositionUnit.Pixels, null, 0, true, "tile_dirt"),
-                new SpriteRenderData().set("1", 0, 0, 0, RenderPositionUnit.Pixels, null, 0, true, "tile_grass"),
-                new SpriteRenderData().set("1", 0, 0, 0, RenderPositionUnit.Pixels, null, 0, true, "tile_mountain")));
-        scheduler.addProcess(new ButtonProcess(renderDataChannel.createOutPort(),
-                inputTouchEventDataChannel.createInPort(), buttonClickEventChannel.createOutPort(),
-                new SpriteRenderData().set("3", 200, 0, 0, RenderPositionUnit.Pixels, null, 0, true, "tile_dirt"),
-                new SpriteRenderData().set("3", 200, 0, 0, RenderPositionUnit.Pixels, null, 0, true, "tile_grass"),
-                new SpriteRenderData().set("3", 200, 0, 0, RenderPositionUnit.Pixels, null, 0, true,
-                        "tile_mountain")));
-        scheduler.addProcess(new ButtonProcess(renderDataChannel.createOutPort(),
-                inputTouchEventDataChannel.createInPort(), buttonClickEventChannel.createOutPort(),
-                new SpriteRenderData().set("4", 0, 200, 0, RenderPositionUnit.Pixels, null, 0, true, "tile_dirt"),
-                new SpriteRenderData().set("4", 0, 200, 0, RenderPositionUnit.Pixels, null, 0, true, "tile_grass"),
-                new SpriteRenderData().set("4", 0, 200, 0, RenderPositionUnit.Pixels, null, 0, true, "tile_mountain")));
-        scheduler.addProcess(new ButtonProcess(renderDataChannel.createOutPort(),
-                inputTouchEventDataChannel.createInPort(), buttonClickEventChannel.createOutPort(),
-                new SpriteRenderData().set("5", 200, 200, 45, RenderPositionUnit.Pixels, null, 0, true, "tile_dirt"),
-                new SpriteRenderData().set("5", 200, 200, 45, RenderPositionUnit.Pixels, null, 0, true, "tile_grass"),
-                new SpriteRenderData().set("5", 200, 200, 45, RenderPositionUnit.Pixels, null, 0, true,
-                        "tile_mountain")));
-//THE BUTTON SHOULD NOW BE PROJECTED - this doesn't work !1
+                new SpriteRenderData("1", 0, 0, 0, RenderPositionUnit.Pixels, null, 0, true, "tile_dirt"),
+                new SpriteRenderData("1", 0, 0, 0, RenderPositionUnit.Pixels, null, 0, true, "tile_grass"),
+                new SpriteRenderData("1", 0, 0, 0, RenderPositionUnit.Pixels, null, 0, true, "tile_mountain")));
+        scheduler.addProcess(
+                new ButtonProcess(renderDataChannel.createOutPort(), inputTouchEventDataChannel.createInPort(),
+                        buttonClickEventChannel.createOutPort(), new SpriteRenderData("3", 200, 0, 0,
+                                RenderPositionUnit.Pixels, null, 0, true, "tile_dirt"),
+                new SpriteRenderData("3", 200, 0, 0, RenderPositionUnit.Pixels, null, 0, true, "tile_grass"),
+                new SpriteRenderData("3", 200, 0, 0, RenderPositionUnit.Pixels, null, 0, true, "tile_mountain")));
+        scheduler.addProcess(
+                new ButtonProcess(renderDataChannel.createOutPort(), inputTouchEventDataChannel.createInPort(),
+                        buttonClickEventChannel.createOutPort(), new SpriteRenderData("4", 0, 200, 0,
+                                RenderPositionUnit.Pixels, null, 0, true, "tile_dirt"),
+                new SpriteRenderData("4", 0, 200, 0, RenderPositionUnit.Pixels, null, 0, true, "tile_grass"),
+                new SpriteRenderData("4", 0, 200, 0, RenderPositionUnit.Pixels, null, 0, true, "tile_mountain")));
+        scheduler.addProcess(
+                new ButtonProcess(renderDataChannel.createOutPort(), inputTouchEventDataChannel.createInPort(),
+                        buttonClickEventChannel.createOutPort(), new SpriteRenderData("5", 200, 200, 45,
+                                RenderPositionUnit.Pixels, null, 0, true, "button_base"),
+                new SpriteRenderData("5", 200, 200, 45, RenderPositionUnit.Pixels, null, 0, true, "button_over"),
+                new SpriteRenderData("5", 200, 200, 45, RenderPositionUnit.Pixels, null, 0, true, "button_down")));
+        // THE BUTTON SHOULD NOW BE PROJECTED - this doesn't work !1
         scheduler.addProcess(new LightweightProcess() {
             final ChannelInPort<ButtonClickEvent> clickEventInPort = buttonClickEventChannel.createInPort();
 
---
+++ desktop/src/net/matthiasauer/stwp4j/libgdx/miniwar/desktop/Tools.java
@@ -0,0 +1,28 @@
+package net.matthiasauer.stwp4j.libgdx.miniwar.desktop;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
+public class Tools {
+	public static void main(String[] args) throws URISyntaxException {
+        String packFileName = "data1.atlas";
+
+	    // get the location of the executed file
+        URI location = Tools.class.getProtectionDomain().getCodeSource().getLocation().toURI();
+        
+        // move up two levels
+        String root = Paths.get(location).getParent().getParent().toString();
+        String androidAssetsFolder = Paths.get(root, "android", "assets").toString();
+        String gfxSourceFolder = Paths.get(root, "gfxSource", packFileName).toString();
+
+        System.out.println("source : " + gfxSourceFolder);
+        System.out.println("target : " + androidAssetsFolder);
+        
+        TexturePacker.process(gfxSourceFolder, androidAssetsFolder, packFileName);
+	}
+}
